7c0eb3a66d39063cc49e92410ad123fcdcbae91b
hadil-rjb <hadilrjeb20@gmail.com>
2024-08-01 14:55:30 +0100

    add Blog preview card

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      boxShadow: {
+        'custom': '8px 8px 0px 1px black',
+      }
+    }
+  },
+  plugins: [],
+}
+
